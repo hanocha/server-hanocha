0799e21521bd20b0a7b0683679a7b0b1406cf22c
Hanocha <saiki@feedforce.jp>
2016-07-03 00:14:55 +0900

    rbenvインストール設定を追加

--- cookbooks/rbenv/recipes/default.rb
+++ cookbooks/rbenv/recipes/default.rb
@@ -6,3 +6,19 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+git '/home/hanocha/.rbenv' do
+  user 'hanocha'
+  group 'hanocha'
+  repository 'git://github.com/sstephenson/rbenv.git'
+  revision 'master'
+end
+
+script 'add_rbenv_path' do
+  interpreter 'bash'
+  user 'hanocha'
+  group 'hanocha'
+  environment "HOME" => "/home/hanocha"
+  code %Q{echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> $HOME/.bash_profile}
+  not_if %Q{grep 'export PATH="$HOME/.rbenv/bin:$PATH"' $HOME/.bash_profile}, environment: { 'HOME' => "/home/hanocha" }
+end
