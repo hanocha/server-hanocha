01770e8f53f8fdddf2bc5433e905200c8b9cca92
Hanocha <saiki@feedforce.jp>
2016-07-03 09:59:33 +0900

    unite some init codes

--- cookbooks/rbenv/recipes/default.rb
+++ cookbooks/rbenv/recipes/default.rb
@@ -14,29 +14,15 @@ git '/home/hanocha/.rbenv' do
   revision 'master'
 end
 
-script 'add rbenv path to .bash_profile' do
+script 'init and install rbenv' do
   interpreter 'bash'
   user 'hanocha'
   group 'hanocha'
   environment "HOME" => "/home/hanocha"
-  code %Q{echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> $HOME/.bash_profile}
+  code <<-EOS
+    echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> $HOME/.bash_profile
+    $HOME/.rbenv/bin/rbenv init
+    echo 'eval "$(rbenv init -)"' >> $HOME/.bash_profile
+  EOS
   not_if %Q{grep '.rbenv/bin' $HOME/.bash_profile}, environment: { 'HOME' => "/home/hanocha" }
-end
-
-script 'exec "rbenv init"' do
-  interpreter 'bash'
-  user 'hanocha'
-  group 'hanocha'
-  environment "HOME" => "/home/hanocha"
-  code %Q{$HOME/.rbenv/bin/rbenv init}
-  returns 1
-end
-
-script 'add rbenv init command to .bash_profile' do
-  interpreter 'bash'
-  user 'hanocha'
-  group 'hanocha'
-  environment "HOME" => "/home/hanocha"
-  code %Q{echo 'eval "$(rbenv init -)"' >> $HOME/.bash_profile}
-  not_if %Q{grep 'eval "$(rbenv init -)"' $HOME/.bash_profile}, environment: { 'HOME' => "/home/hanocha" }
 end
