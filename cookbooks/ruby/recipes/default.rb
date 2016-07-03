8b9351be19a038287542127e246ef45033c8c355
Hanocha <saiki@feedforce.jp>
2016-07-03 10:57:46 +0900

    create ruby install cookbook

--- cookbooks/ruby/recipes/default.rb
+++ cookbooks/ruby/recipes/default.rb
@@ -6,3 +6,10 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+script 'install ruby with rbenv' do
+  interpreter 'bash'
+  code <<-EOS
+    sudo -u hanocha /home/hanocha/.rbenv/bin/rbenv install 2.3.1
+  EOS
+end
