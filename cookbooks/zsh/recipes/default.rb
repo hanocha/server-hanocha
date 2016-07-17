492fd86d95c1fcbd065d28fc3234e6442307c198
Hanocha <saiki@feedforce.jp>
2016-07-17 23:28:01 +0900

    create zsh recipe

--- cookbooks/zsh/recipes/default.rb
+++ cookbooks/zsh/recipes/default.rb
@@ -6,3 +6,16 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+package 'zsh'
+
+script 'install oh-my-zsh' do
+  interpreter 'bash'
+  user 'hanocha'
+  group 'hanocha'
+  environment "HOME" => "/home/hanocha"
+  code <<-EOS
+    sudo -u hanocha sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  EOS
+  not_if %Q{chsh -l | grep zsh}, environment: { 'HOME' => "/home/hanocha" }
+end
