61e10b93b9f9aa9829e7ae6960ab326acad7c189
Hanocha <saiki@feedforce.jp>
2016-07-18 18:07:45 +0900

    delete oh-my-zsh cookbook and unite to zsh cookbook

--- cookbooks/oh-my-zsh/recipes/default.rb
+++
@@ -1,8 +0,0 @@
-#
-# Cookbook Name:: oh-my-zsh
-# Recipe:: default
-#
-# Copyright 2016, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
--- cookbooks/zsh/recipes/default.rb
+++ cookbooks/zsh/recipes/default.rb
@@ -9,13 +9,18 @@
 
 package 'zsh'
 
+user 'hanocha' do
+  action :modify
+  shell '/bin/zsh'
+end
+
 script 'install oh-my-zsh' do
-  interpreter 'bash'
   user 'hanocha'
-  group 'hanocha'
+  interpreter 'bash'
   environment "HOME" => "/home/hanocha"
   code <<-EOS
-    sudo -u hanocha sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -o $HOME/install.sh
+    sh $HOME/install.sh
   EOS
-  not_if %Q{chsh -l | grep zsh}, environment: { 'HOME' => "/home/hanocha" }
+  not_if %Q{test -d .oh-my-zsh}, environment: { 'HOME' => "/home/hanocha" }
 end
